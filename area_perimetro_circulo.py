95a8848efebad9b43174eeedeb0005ce7d37ae76
NicolasWandurragaGomez <nicolaswandurraga@colegioguanenta.edu.co>
2024-02-09 07:39:33 -0500

    se agrego el codico python del programa

---
+++ area_perimetro_circulo.py
@@ -0,0 +1,17 @@
+import math
+
+
+# imput
+r = input("ingrese el radio: ")
+r = int(r)
+
+
+# processing
+a = math.pi * r**2
+p = 2 * math.pi * r
+
+
+# output
+print("El area es: " + str(a))
+print("El perimetro es: " + str(p))
+	
